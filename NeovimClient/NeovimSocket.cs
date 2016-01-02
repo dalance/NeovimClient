5d340a8a2bdb0db69072ddb54b6e513c8cd41204
dalance <dalance@gmail.com>
2016-01-03 02:27:54 +0900

    Add NeovimSocket ( not tested )

---
+++ NeovimClient/NeovimSocket.cs
@@ -0,0 +1,55 @@
+﻿using Mono.Unix;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeovimClient {
+    public class NeovimSocket : NeovimIO {
+
+        // - field -----------------------------------------------------------------------
+
+        private string        neovimSocketPath;
+        private NetworkStream neovimStream;
+
+        // - property --------------------------------------------------------------------
+
+        // - public methods --------------------------------------------------------------
+
+        public NeovimSocket( string socketPath ) {
+            neovimSocketPath = socketPath;
+        }
+
+        public override void Init() {
+            InitSocket( neovimSocketPath );
+            base.Init();
+        }
+
+        public override void Dispose() {
+            try {
+                neovimStream.Dispose();
+            } finally {
+                base.Dispose();
+            }
+        }
+
+        // - event handler ---------------------------------------------------------------
+
+        // - private methods -------------------------------------------------------------
+
+        private void InitSocket( string socketPath ) {
+            var endpoint = new UnixEndPoint( socketPath );
+            var socket = new Socket( AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified );
+            socket.Connect( endpoint );
+            neovimStream = new NetworkStream( socket, true );
+
+            neovimIn  = new StreamWriter( neovimStream );
+            neovimOut = new StreamReader( neovimStream );
+        }
+
+    }
+}
---
+++ Test/TestNeovimSocket.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeovimClient;
+using System.Diagnostics;
+
+namespace Test {
+    [TestClass]
+    public class TestNeovimSocket {
+
+        // This test only works with Mono on UNIX environment
+        /*
+
+        private static NeovimClient<NeovimSocket> nvim;
+        private static Process proc;
+
+        [ClassInitialize]
+        public static void ClassInit( TestContext context ) {
+            var info = new ProcessStartInfo( "nvim", "--embed --headless" );
+            info.CreateNoWindow  = true;
+            info.UseShellExecute = false;
+            info.EnvironmentVariables.Add( "NVIM_LISTEN_ADDRESS", "/tmp/nvim" );
+
+            proc = new Process();
+            proc.StartInfo = info;
+            proc.Start();
+
+            nvim = new NeovimClient<NeovimSocket>( new NeovimSocket( "/tmp/nvim" ) );
+            nvim.Init();
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup() {
+            nvim.Dispose();
+            proc.Dispose();
+        }
+
+        [TestMethod]
+        public void TestVimStrwidth() {
+            var ret = nvim.Func<string, long>( "vim_strwidth", "aaa" );
+            Assert.AreEqual( ret, 3 );
+        }
+        */
+    }
+}
